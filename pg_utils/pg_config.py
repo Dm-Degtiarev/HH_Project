d387013159057e8dfb72ff4c6de96e9fedafe66c
Dmitrii Degtiarev <9774385859@mail.ru>
2023-04-21 22:51:43 +0300

    Сделал чтобы пароль для подключения брался из переменной среды - PG_PASSWORD

--- pg_utils/pg_config.py
+++ pg_utils/pg_config.py
@@ -1,5 +1,10 @@
+import os
+
+# Берем пароль из переменной среды 'PG_PASSWORD'
+pg_password = os.getenv('PG_PASSWORD')
+
 # Вводим данные для подключения к БД
 host = 'localhost'
 user = 'postgres'
-password = 'password'
-db_name = 'postgres'
+password = pg_password
+db_name = 'main'
